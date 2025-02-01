6c4f0fd74d51d39651f5679ede815102de47a32b
Henry Kupty <hkupty@gmail.com>
2025-02-01 12:44:27 +0100

    refactor: restructure signal handling

--- cmd/meadhall/main.go
+++ cmd/meadhall/main.go
@@ -1,9 +1,22 @@
+// Meadhall daemon, runs indefinitely until receiving a signal to stop
+// This package doesn't hold any logic and only deals with handling the binary-to-OS interface.
+// Most notably, the signal handling and any parameter to config (eventual CLI flags or environment variables)
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	pkg "github.com/hkupty/meadhall/pkg/meadhall"
 )
 
 func main() {
-	pkg.Main()
+	appContext, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	pkg.Main(appContext)
+
+	<-appContext.Done()
 }
